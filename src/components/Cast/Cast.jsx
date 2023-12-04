0bb78aebc133a6a4c50cbd96eea1844c3d6ed0a2
LarisaYakovenko <133209659+LarisaYakovenko@users.noreply.github.com>
2023-12-04 19:34:54 +0200

    Update Cast.jsx

--- src/components/Cast/Cast.jsx
+++ src/components/Cast/Cast.jsx
@@ -22,7 +22,7 @@ const Cast = () => {
             <img
               src={
                 profile_path
-                  ? `http://image.tmdb.org/t/p/w500${profile_path}`
+                  ? `https://image.tmdb.org/t/p/w500${profile_path}`
                   : defaultImg
               }
               width={250}
